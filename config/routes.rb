f23bff723dd54d1dd46f4702f266a4bbbd9c1e12
Parisara Wongsethanoonoi <parisara.w@gmail.com>
2022-04-25 19:06:52 +1000

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-# verb "url", to: "controller#action"
-  get 'ask', to: 'questions#ask', as: :ask
-  get 'answer', to: 'questions#answer'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
